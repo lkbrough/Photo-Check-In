91c321fce12a9f9153b5bb2f3ce8044fdd1e4000
DJMartinez1 <domingo.martinez01@utrgv.edu>
2018-12-12 07:25:56 -0600

    Allow for timesheet editing

--- timecards.rb
+++ timecards.rb
@@ -5,6 +5,7 @@ class TimeCard
     property :id, Serial
     property :user_id, Integer
     property :complete, Boolean, :default => false
+    property :is_bold, Boolean, :default => false
     property :sign_in, DateTime
     property :sign_out, DateTime
     property :date, Date
